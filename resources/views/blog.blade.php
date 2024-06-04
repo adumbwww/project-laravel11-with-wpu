b35930f1fdf1e14e789f7d051b64ec5120e92028
Adumb19 <adamwidianto19@gmail.com>
2024-06-05 00:01:26 +0700

    .

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,182 +1,69 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <article class="py-5 max-w-screen-md border-b border-gray-300">
+    <h3 class="mb-2 text-2xl tracking-tight font-bold text-gray-800">Zenless Zone Zero: Perilisan Resmi Dijadwalkan Juli 2024</h3>
+    <div class="text-base text-gray-500">
+      <a href="#">Adam Widianto</a> | 1 January 2024
+    </div>
+    <img src="img/zzz-banner.jpeg" alt="zzz-banner">
+    <p class="my-4 font-light">
+      Para penggemar action RPG dengan elemen roguelike, bersiaplah! Zenless Zone Zero, game terbaru dari HoYoverse, 
+      akan resmi diluncurkan pada awal Juli 2024 untuk berbagai platform, termasuk Android, iOS, PC, dan PlayStation 5.
+      Zenless Zone Zero mengajak pemain untuk menjelajahi New Eridu, sebuah kota metropolitan yang dikelilingi oleh area 
+      berbahaya yang dikenal sebagai Belahan, penuh dengan monster mengerikan. Sebagai pemimpin tim elit, pemain bertugas 
+      menyelamatkan kota dan mengungkap misteri di balik Calamity, peristiwa apokaliptik yang mengubah dunia.
+    </p>
+    <a href="" class="font-medium text-gray-500 hover:underline">Read more &raquo;</a>
+    <figure>
+      <img src="/img/zzz-gameplay.jpg" alt="zzz-gameplay">
+      <figcaption>Zenless Zone Zero Gameplay</figcaption>
+    </figure>
+    <p class="my-4 font-light">
+      Game ini menawarkan perpaduan gameplay yang seru dan menantang, menggabungkan elemen action RPG dengan roguelike. 
+      Pemain akan bertarung melawan berbagai macam monster dengan kemampuan unik dan strategi tim yang diperlukan untuk mengalahkan mereka.
+    </p>
+    <figure>
+      <img src="/img/zzz-mc.jpg" alt="main character">
+      <figcaption>Main Character</figcaption>
+    </figure>
+    <p class="my-4 font-light">
+      Zenless Zone Zero tidak hanya menawarkan gameplay yang menarik, tetapi juga komunitas yang aktif dan penuh dengan pemain yang antusias. 
+      Pemain dapat bergabung dengan berbagai guild, bertukar strategi, dan bersenang-senang bersama pemain lain dari seluruh dunia.
+      Pra-registrasi untuk Zenless Zone Zero telah dibuka di semua platform. Bagi para penggemar action RPG dan game dengan cerita 
+      yang menarik, Zenless Zone Zero adalah game yang wajib dicoba.
+    </p>
+  </article>
 
-
-  <!DOCTYPE html>
-  <html lang="en" class="h-full bg-gray-100">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      @vite('resources/css/app.css')
-      <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-      <title>Document</title>
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-      <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body class="h-full">
-  <div class="min-h-full">
-    <nav class="bg-gray-800" x-data="{ isOpen: false }">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 items-center justify-between">
-          <div class="flex items-center">
-            <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
-            </div>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-                <a href="/blog" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog</a>
-                <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
-              </div>
-            </div>
-          </div>
-          <div class="hidden md:block">
-            <div class="ml-4 flex items-center md:ml-6">
-              <!-- Profile dropdown -->
-              <div class="relative ml-3">
-                <div>
-                  <button type="button"  @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                    <span class="absolute -inset-1.5"></span>
-                    <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                  </button>
-                </div>
-                <div x-show="isOpen"
-                x-transition:enter="transition ease-out duration-100 transform"
-                x-transition:enter-start="opacity-0 scale-95"
-                x-transition:enter-end="opacity-100 scale-100"
-                x-transition:leave="transition ease-in duration-75 transform"
-                x-transition:leave-start="opacity-100 scale-100"
-                x-transition:leave-end="opacity-0 scale-95"
-                class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg"
-                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                  <!-- Active: "bg-gray-100", Not Active: "" -->
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="-mr-2 flex md:hidden">
-            <!-- Mobile menu button -->
-            <button type="button" @click="isOpen = !isOpen" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
-              <span class="absolute -inset-0.5"></span>
-              <span class="sr-only">Open main menu</span>
-              <!-- Menu open: "hidden", Menu closed: "block" -->
-              <svg :class="{'hidden': isOpen, 'block': !isOpen }"
-              class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-              <!-- Menu open: "block", Menu closed: "hidden" -->
-              <svg :class="{'block': isOpen, 'hidden': !isOpen }"
-              class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-  
-      <!-- Mobile menu, show/hide based on menu state. -->
-      <div x-show="isOpen" class="md:hidden" id="mobile-menu">
-        <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-          <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-          <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-          <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
-          <a href="/blog" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Blog</a>
-          <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
-        </div>
-        <div class="border-t border-gray-700 pb-3 pt-4">
-          <div class="flex items-center px-5">
-            <div class="flex-shrink-0">
-              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            </div>
-            <div class="ml-3">
-              <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-              <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
-            </div>
-          </div>
-          <div class="mt-3 space-y-1 px-2">
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-  
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blog Page</h1>
-      </div>
-    </header>
-  
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
-        <article>
-          <h3>Zenless Zone Zero: Perilisan Resmi Dijadwalkan Juli 2024</h3>
-          <img src="img/zzz-banner.jpeg" alt="zzz-banner">
-          <p>
-            Para penggemar action RPG dengan elemen roguelike, bersiaplah! Zenless Zone Zero, game terbaru dari HoYoverse, 
-            akan resmi diluncurkan pada awal Juli 2024 untuk berbagai platform, termasuk Android, iOS, PC, dan PlayStation 5.
-            Zenless Zone Zero mengajak pemain untuk menjelajahi New Eridu, sebuah kota metropolitan yang dikelilingi oleh area 
-            berbahaya yang dikenal sebagai Belahan, penuh dengan monster mengerikan. Sebagai pemimpin tim elit, pemain bertugas 
-            menyelamatkan kota dan mengungkap misteri di balik Calamity, peristiwa apokaliptik yang mengubah dunia.
-          </p>
-          <figure>
-            <img src="/img/zzz-gameplay.jpg" alt="zzz-gameplay">
-            <figcaption>Zenless Zone Zero banner</figcaption>
-          </figure>
-          <p>
-            Game ini menawarkan perpaduan gameplay yang seru dan menantang, menggabungkan elemen action RPG dengan roguelike. 
-            Pemain akan bertarung melawan berbagai macam monster dengan kemampuan unik dan strategi tim yang diperlukan untuk mengalahkan mereka.
-          </p>
-          <figure>
-            <img src="/img/zzz-mc.jpg" alt="main character">
-            <figcaption>Main Character</figcaption>
-          </figure>
-          <p>
-            Zenless Zone Zero tidak hanya menawarkan gameplay yang menarik, tetapi juga komunitas yang aktif dan penuh dengan pemain yang antusias. 
-            Pemain dapat bergabung dengan berbagai guild, bertukar strategi, dan bersenang-senang bersama pemain lain dari seluruh dunia.
-            Pra-registrasi untuk Zenless Zone Zero telah dibuka di semua platform. Bagi para penggemar action RPG dan game dengan cerita 
-            yang menarik, Zenless Zone Zero adalah game yang wajib dicoba.
-          </p>
-        </article>
-      
-        <article>
-          <h3>Wuthering Waves: Siap Berpetualang di Dunia Terbuka yang Menakjubkan!</h3>
-          <img src="img/ww-banner.jpg" alt="banner">
-          <p>
-            Para penjelajah dunia fantasi, bersiaplah untuk petualangan baru yang luar biasa! Wuthering Waves, game open-world action RPG 
-            terbaru dari Kuro Games, telah resmi diluncurkan pada 22 Mei 2024 untuk platform PC, Android, dan iOS. Sejak perilisannya, 
-            game ini telah mendapatkan banyak pujian dari para pemain karena dunia yang luas dan indah, gameplay yang seru dan menantang, 
-            serta cerita yang menarik
-          </p>
-          <figure>
-            <img src="img/ww-gameplay.jpg" alt="gamplay-trailer">
-            <figcaption>Wuthering Waves Trailer</figcaption>
-          </figure>
-          <p>
-            Memasuki Wuthering Waves, pemain akan disuguhkan dengan dunia yang sangat luas dan penuh dengan tempat-tempat menarik untuk dikunjungi. 
-            Dari hutan lebat yang penuh dengan monster berbahaya, hingga kota-kota yang ramai dengan penduduk yang ramah, setiap sudut dunia ini 
-            memiliki cerita dan misterinya sendiri. Pemain bebas untuk menjelajahi dunia ini sesuka hati mereka, dan selalu ada sesuatu yang baru untuk ditemukan.
-          </p>
-          <figure>
-            <img src="img/ww-mc.jpg" alt="mc">
-            <figcaption>Main Character</figcaption>
-          </figure>
-          <p>
-            Wuthering Waves adalah game open-world RPG yang wajib dicoba bagi para gamer yang mencari pengalaman bermain yang imersif, visual yang memukau, 
-            gameplay yang seru, dan cerita yang menarik.
-          </p>
-        </article>
-      </div>
-    </main>
-  </div>
-  
-  </body>
-  </html>
-    
+  <article class="py-5 max-w-screen-md border-b border-gray-300">
+    <h3 class="mb-2 text-2xl tracking-tight font-bold text-gray-800">Wuthering Waves: Siap Berpetualang di Dunia Terbuka yang Menakjubkan!</h3>
+    <div class="text-base text-gray-500">
+      <a href="#">Adam Widianto</a> | 2 February 2024
+    </div>
+    <img src="img/ww-banner.jpg" alt="banner">
+    <p class="my-4 font-light">
+      Para penjelajah dunia fantasi, bersiaplah untuk petualangan baru yang luar biasa! Wuthering Waves, game open-world action RPG 
+      terbaru dari Kuro Games, telah resmi diluncurkan pada 22 Mei 2024 untuk platform PC, Android, dan iOS. Sejak perilisannya, 
+      game ini telah mendapatkan banyak pujian dari para pemain karena dunia yang luas dan indah, gameplay yang seru dan menantang, 
+      serta cerita yang menarik
+    </p>
+    <a href="" class="font-medium text-gray-500 hover:underline">Read more &raquo;</a>
+    <figure>
+      <img src="img/ww-gameplay.jpg" alt="gamplay-trailer">
+      <figcaption>Wuthering Waves Trailer</figcaption>
+    </figure>
+    <p class="my-4 font-light">
+      Memasuki Wuthering Waves, pemain akan disuguhkan dengan dunia yang sangat luas dan penuh dengan tempat-tempat menarik untuk dikunjungi. 
+      Dari hutan lebat yang penuh dengan monster berbahaya, hingga kota-kota yang ramai dengan penduduk yang ramah, setiap sudut dunia ini 
+      memiliki cerita dan misterinya sendiri. Pemain bebas untuk menjelajahi dunia ini sesuka hati mereka, dan selalu ada sesuatu yang baru untuk ditemukan.
+    </p>
+    <figure>
+      <img src="img/ww-mc.jpg" alt="mc">
+      <figcaption>Main Character</figcaption>
+    </figure>
+    <p class="my-4 font-light">
+      Wuthering Waves adalah game open-world RPG yang wajib dicoba bagi para gamer yang mencari pengalaman bermain yang imersif, visual yang memukau, 
+      gameplay yang seru, dan cerita yang menarik.
+    </p>
+  </article>
+</x-layout>
+        
